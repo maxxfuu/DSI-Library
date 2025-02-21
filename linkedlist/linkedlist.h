a7b7b59f8e76c9172a86644c24e29f1e7c195be4
maxxfuu <maxfu826@gmail.com>
2025-02-21 02:00:20 -0800

    ll

--- linkedlist/linkedlist.h
+++ linkedlist/linkedlist.h
@@ -1,28 +1,64 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-template<typename T>
-struct Node {
-    T data;
-    Node<T>* next;
-
-    Node(const T& value) : data(value), next(nullptr) {
-    }
-};
+#include <iostream>
 
 template<typename T>
-class Linkedlist {
+class LinkedList{
 private:
-Node<T>* head;
-Node<T>* tail;
+    struct Node{
+        T data;
+        Node* next;
+
+        // Default Constrcutor for structs
+        Node(const T& val) : data(val), next(nullptr){};
+    };
+    
+    // Data Members
+    Node* head;
+    Node* tail;
+    
+    void clear() {
+        // Free Memory
+        while(head) {
+            Node* temp = head; // New ptr pointing to the same memory location as head, which is the first node.
+            head = head->next;
+            delete temp;
+        }
+        tail = nullptr;
+    }
+
+    void deepCopy(const LinkedList& other) {
+        Node* current = other.head;
+        while(current) {
+            insertAtEnd();
+        }
+    }
 
 public:
 
     // Default Constructor
-    Linkedlist() : head(nullptr), tail(nullptr) {}
+    LinkedList() : head(nullptr), tail(nullptr){}
+    
+    // Copy Constuctor (Deep Copy)
+    LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr) {}
+    
+    LinkedList& operator=(const LinkedList& other) {
+        if (this != &other) {
+            clear();
+        }
 
-    // Destructor
-    ~Linkedlist() {}
+    }
+
+    ~LinkedList() {
+    }
+    
+    void insertAtEnd(const T& value) {
+        Node* newNode = new Node(value);
+        newNode->data = value;
+        newNode->next 
+    }
+
 };
 
 #endif
