a167f5df853183eac1904b1b7a8be53969b0873f
maxxfuu <maxfu826@gmail.com>
2025-03-09 01:56:17 -0800

    unqiue ptr completed

--- unique_ptr/unique_ptr.cpp
+++ unique_ptr/unique_ptr.cpp
@@ -2,5 +2,10 @@
 
 int main() {
     Resource* obj1 = new Resource();
-    return 0; // Resource never destroyed. Use, unqiue_ptr to destroy when obj is out of scope.
+
+    UniquePtr<Resource> up(new Resource());
+
+    return 0; // obj1 never destroyed. Use, unqiue_ptr to destroy when obj is out of scope.
 }
+
+
--- unique_ptr/unique_ptr.h
+++ unique_ptr/unique_ptr.h
@@ -5,17 +5,38 @@
 
 class Resource {
 public:
-    Resource() {std::cout << "Resource Created";}
-    ~Resource() {std::cout << "Resource Deleted";}
+    
+    Resource() { printf("Resouce %p Created\n", this); }
+    ~Resource() { printf("Resouce %p Deleted\n", this); }
 };
 
+template<typename T>
 class UniquePtr {
-public:
-    UniquePtr() {
-
-    }
 private:
+    T* pointer_;
 
+public:
+    UniquePtr(T* p = nullptr) : pointer_(p) {}
+    ~UniquePtr() { delete pointer_; }
+    
+    // Move Semantics
+    UniquePtr(UniquePtr&& other) : pointer_(other.pointer_){
+        other.pointer_ = nullptr;
+    }
+
+    UniquePtr& operator=(UniquePtr&& other) {
+        if (this != &other) {
+            delete pointer_;
+            this->pointer_ = other.pointer_;
+            other.pointer_ = nullptr;
+        }
+        return *this;
+    }
+    
+    // Member Functions
+    T& operator*() const { return *pointer_; }
+    T* operator->() const { return pointer_; }
+    
 };
 
 #endif
